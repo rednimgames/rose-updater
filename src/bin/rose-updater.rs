190dd4be94f79bc95a547cbd16a430e8c0840e14
Ralph Minderhoud <mail@ralphminderhoud.com>
2024-02-29 00:29:31 -0500

    Remove legacy checkbox

--- src/bin/rose-updater.rs
+++ src/bin/rose-updater.rs
@@ -32,11 +32,6 @@ const UPDATER_OLD_EXT: &str = "old";
 
 const TEXT_FILE_EXTENSIONS: &[&str; 1] = &["xml"];
 
-#[derive(Default, Serialize, Deserialize)]
-struct Settings {
-    use_legacy_client: bool,
-}
-
 #[derive(Clone, Parser, Debug)]
 #[clap(about, version, author)]
 struct Args {
@@ -488,25 +483,6 @@ fn main() -> anyhow::Result<()> {
             .expect("Critical failure: Failed to set default tracing subscriber");
     }
 
-    // Load application settings
-    let settings_dir = directories::ProjectDirs::from("", "Rednim Games", "ROSE Online")
-        .and_then(|d| Some(d.config_dir().to_owned()))
-        .unwrap_or(PathBuf::from("."));
-
-    let settings_path = settings_dir.join("rose-updater.json");
-    tracing::info!(path =? settings_path.display(), "Loading updater settings");
-
-    let mut settings = if let Ok(saved_settings) = std::fs::read(&settings_path)
-        .map_err(|e| anyhow::anyhow!(e))
-        .and_then(|data| Ok(serde_json::from_slice(&data)?))
-    {
-        saved_settings
-    } else {
-        // Remove the bad json file if it exists
-        let _ = std::fs::remove_file(&settings_path);
-        Settings::default()
-    };
-
     // Load application resources
     let icon_bytes = include_bytes!("../../res/client.png");
     let background_bytes = include_bytes!("../../res/Launcher_Alpha_Background.png");
@@ -529,13 +505,6 @@ fn main() -> anyhow::Result<()> {
 
     let mut launch_button = launch_button::LaunchButton::new(572, 547);
     launch_button.deactivate();
-
-    let mut legacy_checkbox =
-        fltk::button::CheckButton::new(610, 605, 120, 20, "Use Legacy Client");
-    legacy_checkbox.set_label_color(fltk::enums::Color::White);
-    legacy_checkbox.set_label_type(fltk::enums::LabelType::Shadow);
-    legacy_checkbox.set_checked(settings.use_legacy_client);
-    legacy_checkbox.set_tooltip("Enable the old legacy client when starting the game. Leaving this unchecked will launch the old version");
 
     let mut webview_win = window::Window::default().with_size(780, 530).with_pos(0, 0);
     webview_win.set_border(false);
@@ -591,21 +560,6 @@ fn main() -> anyhow::Result<()> {
     let exe_dir = args.exe_dir.clone();
     let exe_args = args.exe_args.clone();
 
-    let use_legacy = Rc::new(RefCell::new(settings.use_legacy_client));
-    {
-        let use_legacy = use_legacy.clone();
-        legacy_checkbox.set_callback(move |legacy_checkbox| {
-            *use_legacy.borrow_mut() = legacy_checkbox.is_checked();
-
-            settings.use_legacy_client = legacy_checkbox.is_checked();
-
-            let _ = std::fs::create_dir_all(&settings_dir);
-            if let Ok(data) = serde_json::to_vec(&settings) {
-                let _ = std::fs::write(&settings_path, data);
-            };
-        });
-    }
-
     // When the launch button is clicked we start the application
     launch_button.set_callback(move |_| {
         info!(
@@ -615,12 +569,7 @@ fn main() -> anyhow::Result<()> {
             exe_args.join(" ")
         );
 
-        let use_legacy = *use_legacy.borrow();
-        let exe = if use_legacy {
-            exe_dir.join("trose-legacy.exe")
-        } else {
-            exe_dir.join(&exe)
-        };
+        let exe = exe_dir.join(&exe);
 
         Command::new(&exe)
             .current_dir(&exe_dir)
@@ -670,7 +619,6 @@ fn main() -> anyhow::Result<()> {
                         background_frame.redraw();
                         main_progress_bar.redraw();
                         launch_button.redraw();
-                        legacy_checkbox.redraw();
                     }
                     MainProgressUpdaterEvent::IncrementProgress(amount) => {
                         main_progress_bar.set_value(main_progress_bar.value() + amount);
